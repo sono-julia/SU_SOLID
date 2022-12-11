dfe51a31ae2710c28678de7a51633b9576c8a6dd
Yuliya Suvorova <julios1994@mail.ru>
2022-12-11 22:33:04 +0500

    Решение задачи о палиндроме

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Solid
+{
+    class Solution
+    {
+        //https://leetcode.com/problems/palindrome-number/
+        static void Main()
+        {
+            string testPalindrom = Console.ReadLine();
+            Console.WriteLine(IsPalindrom(testPalindrom).ToString());
+        }
+        /// <summary>
+        /// Является ли строка палиндромом.
+        /// </summary>
+        /// <param name="str">Строка для проверки.</param>
+        /// <returns></returns>
+        static bool IsPalindrom(string str)
+        {
+            bool test = true;
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                test = str[i] == str[j];
+                if (!test) { break; }
+            }
+            return test;
+        }
+        static bool IsPalindrom(int x)
+        {
+            string str = x.ToString();
+            bool test = true;
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                test = str[i] == str[j];
+                if (!test) { break; }
+            }
+            return test;
+        }
+    }
+}
