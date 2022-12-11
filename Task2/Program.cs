ec382c851ad587f9928442de02090644d3a9adb4
Yuliya Suvorova <julios1994@mail.ru>
2022-12-11 23:54:06 +0500

    Добавлено решение второго задания (римские цифры)

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,7 +5,50 @@
         //https://leetcode.com/problems/roman-to-integer/
         static void Main()
         {
-
+            string romanNumber = Console.ReadLine();
+            Console.WriteLine(GetNumberFromRome(romanNumber));
+        }
+        /// <summary>
+        /// Получить десятичную запись римского числа.
+        /// </summary>
+        /// <param name="romanNumber">Римское число.</param>
+        /// <returns>Целое число в десятичной записи.</returns>
+        static int GetNumberFromRome(string romanNumber)
+        {
+            int resultNumber = 0;
+            Dictionary<char, int> map = new Dictionary<char, int>
+            {
+                {'I', 1 },
+                {'V', 5 },
+                {'X', 10 },
+                {'L', 50 },
+                {'C', 100 },
+                {'D', 500 },
+                {'M', 1000 },
+            };
+            int temp = map[romanNumber.First()];
+            bool firstStep = true;
+            foreach (char nRome in romanNumber)
+            {
+                if (firstStep)
+                {
+                    firstStep = false;
+                }
+                else
+                {
+                    int curNumber = map[nRome];
+                    if (curNumber > temp)
+                    {
+                        resultNumber -= temp;
+                    }
+                    else
+                    {
+                        resultNumber += temp;
+                    }
+                    temp = curNumber;
+                }
+            }
+            return resultNumber + temp;
         }
     }
 }
