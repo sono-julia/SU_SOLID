eff12605677c641e0a8647c83bd22a7ebdf13e8e
Yuliya Suvorova <julios1994@mail.ru>
2022-12-12 18:22:21 +0500

    Добавлено решение 6 задания (Substring)

---
+++ Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Solid
+{
+    class Solution
+    {
+        //https://leetcode.com/problems/is-subsequence/solutions/
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+            Console.WriteLine(s);
+        }
+
+        static bool IsSubstring(string s, string t)
+        {
+            int i = 0;
+            foreach (char c in t)
+            {
+                if (c.Equals(s[i]))
+                {
+                    i++;
+                }
+                if (i == s.Length)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
