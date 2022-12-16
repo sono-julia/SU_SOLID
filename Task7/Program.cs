901ac8b55cb1430979c9670909f96d8b6097ae62
Yuliya Suvorova <julios1994@mail.ru>
2022-12-16 23:16:31 +0500

    Решение задания 7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -6,13 +6,37 @@
         static void Main()
         {
             int[] s = Console.ReadLine().Split().Select(n=>int.Parse(n)).OrderBy(n=>n).ToArray();
-            Console.WriteLine(String.Join(" ", s));
-            
+            //Console.WriteLine(String.Join(" ", s));
+            Console.WriteLine(string.Join(" ",FindDisappearedNumbers(s)));
         }
 
-        //public IList<int> FindDisappearedNumbers(int[] nums)
-        //{
-            
-        //}
+
+        //226 ms
+        public static IList<int> FindDisappearedNumbers_(int[] nums)
+        {
+            int length = nums.Length;
+            HashSet<int> unique = nums.ToHashSet();
+            IEnumerable<int> indexes = Enumerable.Range(1, length);
+            List<int> res = indexes.Except(unique).ToList();
+            return res;
+        }
+
+        //147 ms
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            Array.Sort(nums);
+            int length = nums.Length;
+            List<int> res = new List<int>();
+            nums.Distinct();
+
+            for (int i = 1; i <= length; i++)
+            {
+                if (!nums.Contains(i))
+                {
+                    res.Add(i);
+                }
+            }
+            return res;
+        }
     }
 }
